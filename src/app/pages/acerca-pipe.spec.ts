71a6de339a53281cf2ebfc5c7b3df5844d379af1
Tony <jaespinosa2099@gmail.com>
2026-02-24 15:40:10 -0600

    Initial commit

---
+++ src/app/pages/acerca-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { AcercaPipe } from './acerca-pipe';
+
+describe('AcercaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new AcercaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
